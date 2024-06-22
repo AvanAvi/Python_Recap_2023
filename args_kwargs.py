6debab289ad2f40467ad088dd0ce143de730c17a
AvanAvi <theastrofreek@gmail.com>
2024-06-22 05:05:24 +0200

    args and kwargs

--- args_kwargs.py
+++ args_kwargs.py
@@ -1,8 +1,12 @@
-def add(*args):
-    print(sum(args))
+def add(*args,**kwrgs):
+    for key , value in kwrgs.items():
+        print(f"{key} : {value}")
+    
 
 
 
 add(12,2,82,92)
 add(1,2,3,4,5,6,7,8,9,)
+add(name="avan",age=25,gender="male")
+
 
