b7bbdcbff8c779cbd0d9deb2354a8b75f08b5772
AvanAvi <theastrofreek@gmail.com>
2024-06-22 17:18:21 +0200

    more args and kwargs

---
+++ args_kwargs2.py
@@ -0,0 +1,10 @@
+def add(n1,n2,n3,*args,**kwargs):
+    print(f"{n1=}")
+    print(f"{n2=}")
+
+    print(f"{n3=}")
+    print(f"{args=}")
+    print(f"{kwargs=}")
+
+
+add(12,13,12,83,73,name="avan")
